4407a6b2e920fb310e0a872fc8a7db9307cb0fe4
George Stephanis <daljo628@gmail.com>
2026-03-23 08:50:04 -0400

    Add built Pardot block assets and async webpack entry
    Add compiled build assets for pardot-form and pardot-field blocks (JS, CSS, RTL styles, and asset manifests). Update webpack.config.js to use an async entry function and include new entry points for attribution and admin-bar-attribution so those scripts are emitted alongside the default entries.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
 module.exports = {
 	...defaultConfig,
-	entry: {
-		...defaultConfig.entry,
+	entry: async () => ( {
+		...( await defaultConfig.entry() ),
 		attribution: './src/attribution.js',
 		'admin-bar-attribution': './src/admin-bar-attribution.js',
-	},
+	} ),
 };
